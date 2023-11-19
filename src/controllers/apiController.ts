89ff646d8d5bd0b3dd411a2ce8297d2813a787dd
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-11-18 23:30:15 -0300

    cheio de up
    agora aprendi o CRUD com MySQL

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,5 +1,6 @@
 import { Request, Response} from 'express'
 import { Phrase } from '../models/Phrase'
+import { Sequelize } from 'sequelize'
 
 
 export const ping = async (req: Request, res: Response) => {
@@ -19,12 +20,15 @@ export const name = (req:Request, res:Response) => {
 }
 
 export const createPhrase = async (req: Request, res: Response) => {
+    
     let { author, txt } = req.body
-
-    let phrase = Phrase.build({ author, txt})
-    console.log(phrase)
-    await phrase.save()
-    res.json({ ok: "ok?"})
+    let newPhrase = await Phrase.create( { author, txt })
+    res.status(201)
+    res.json({ id: newPhrase.id, author, txt })
+    // let phrase = Phrase.build({ author, txt})
+    // console.log(phrase)
+    // await phrase.save()
+    // res.json({ ok: "ok?"})
     // try {
     //     let { author } = req.body
     //     const  newPhrase = Phrase.create( { author})
@@ -51,4 +55,56 @@ export const getPhrases =async (req: Request, res:Response) => {
         let frases = await Phrase.findAll()
         console.log(frases)
         res.json(frases)
+}
+
+export const getOnePhrase =async (req:Request, res:Response) => {
+    
+    let frase = await Phrase.findByPk(req.params.id)
+    if (frase) {
+        console.log(frase.txt)
+        res.status(200)
+        res.json({ frase: frase.txt })
+    } else {
+        res.status(404)
+        res.json({ error: 'Frase nao encontrada'})
+    }
+}
+
+export const updatePhrase =async (req:Request, res:Response) => {
+    let { id } = req.params
+    let { author, txt } = req.body
+    let phrase = await Phrase.findByPk(id)
+    if (phrase) {
+        phrase.author = author
+        phrase.txt = txt
+        await phrase.save()
+        res.json({ phrase })
+    } else {
+        res.json({ error: 'Frase nao encontrada'})
+
+    }
+
+}
+
+export const deletePhrase =async (req:Request, res:Response) => {
+    
+    let { id } = req.params
+
+    await Phrase.destroy({ where: {id}})
+
+    res.json({})
+}
+
+export const randomPhrase =async (req:Request, res: Response) => {
+    let phrase = await Phrase.findOne({
+        order: [
+            Sequelize.fn('RAND')
+        ]
+    })
+
+    if(phrase) {
+        res.json(phrase)
+    } else{
+        res.json({ error: 'Não há frases cadastradas.' })
+    }
 }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,7 +7,12 @@ const router = Router()
 router.get('/ping', ApiController.ping)
 router.get('/random', ApiController.random)
 router.get('/nome/:nome', ApiController.name)
+
 router.post('/frases', ApiController.createPhrase)
 router.get('/frases', ApiController.getPhrases)
+router.get('/frases/aleatoria', ApiController.randomPhrase)
+router.get('/frases/:id', ApiController.getOnePhrase)
+router.put('/frases/:id', ApiController.updatePhrase)
+router.delete('/frases/:id', ApiController.deletePhrase)
 
 export default router
